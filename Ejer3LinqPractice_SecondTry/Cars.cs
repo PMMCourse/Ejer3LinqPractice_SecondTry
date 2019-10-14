f0c8eddf27832d87d0aff13069444873aa64a4b1
Emmanuel <emmanuel@emmanuel.es>
2019-10-14 10:51:44 +0200

    Todo preparado para empezar.

---
+++ Ejer3LinqPractice_SecondTry/Cars.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer3LinqPractice_SecondTry
+{
+    class Cars
+    {
+        public int id { get; set; }
+
+        public string Maker { get; set; }
+
+        public string Model { get; set; }
+
+        public int Year { get; set; }
+
+        public string Color { get; set; }
+
+        public struct Location
+        {
+            public double Latitude { get; set; }
+            public double Longitude { get; set; }
+        }
+    }
+}
--- Ejer3LinqPractice_SecondTry/Program.cs
+++ Ejer3LinqPractice_SecondTry/Program.cs
@@ -3,13 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace Ejer3LinqPractice_SecondTry
 {
     class Program
     {
+        /*
+         * ENUNCIADOS
+         * 
+         4.- Muestra fabricante y modelo. Del color Fuscia.
+         5.- Permite al usuario introducir una latitud y una longitud. Indica al usuario si encuentra un coche de color Turquoise dentro de esa latitud y longitud facilitada.
+         7.- Genera una nueva clase con modelo y fabricante. Muestra todos los coches que no tengan latitud, ni longitud Convierte en la búsqueda a esa clase.
+        10.- Agrupa todos los coches por fabricante, muestra los colores disponibles sin duplicar la muestra.
+         */
+
+            public static List<Cars> lista()
+        {
+            String ruta = Path.GetFullPath("Cars.json");//Obtengo de la ruta relativa la absoluta
+            StreamReader jsonStream = File.OpenText(ruta);
+            var json = jsonStream.ReadToEnd();
+            //lo decodifico del formato JSON y lo guardo en una lista de heroes
+            List<Cars> coches = JsonConvert.DeserializeObject<List<Cars>>(json);
+            return coches;
+        }
+
+        public void EJer4()
+        {
+
+        }
+
+        public void EJer5()
+        {
+
+        }
+        public void EJer7()
+        {
+
+        }
+        public void EJer10()
+        {
+
+        }
         static void Main(string[] args)
         {
+            //EJer4
+            //EJer5
+            //EJer7
+            //EJer10
         }
     }
 }
