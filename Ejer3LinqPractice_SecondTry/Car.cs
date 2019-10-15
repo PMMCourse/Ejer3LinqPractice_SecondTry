903450c1028d118b1ea73d2892ca8c0b9961572e
DaniRomeroR <daniel.romero.rodriguez2@gmail.com>
2019-10-15 10:24:54 +0200

    primer ejercicio

---
+++ Ejer3LinqPractice_SecondTry/Car.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer3LinqPractice_SecondTry
+{
+    class Car
+    {
+        public int id { get; set; }
+        public string Maker { get; set; }
+        public string Model { get; set; }
+        public int? Year { get; set; }
+        public string Color { get; set; }
+        public Location location { get; set; }
+
+        public struct Location
+        {
+            public double? Latitude { get; set; }
+            public double? Longitude { get; set; }
+        }
+    }
+}
--- Ejer3LinqPractice_SecondTry/Program.cs
+++ Ejer3LinqPractice_SecondTry/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Ejer3LinqPractice_SecondTry
 {
@@ -10,6 +12,21 @@ namespace Ejer3LinqPractice_SecondTry
     {
         static void Main(string[] args)
         {
+            String archivo = File.ReadAllText("Cars.json");
+            List<Car> coches = JsonConvert.DeserializeObject<List<Car>>(archivo);
+            Ejer4(coches);
+            Console.ReadKey();
         }
+
+        public static void Ejer4(List<Car> coche) {
+            var ej4 = coche.Where(x => x.Color=="Fuscia");
+
+            foreach (var a in ej4)
+            {
+                    Console.WriteLine($"Fabricante:{a.Maker}  Modelo:{a.Model}");
+            }
+        }
+
+
     }
 }
