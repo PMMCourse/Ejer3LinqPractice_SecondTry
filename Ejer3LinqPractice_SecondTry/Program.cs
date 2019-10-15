5d637ad69d79f597d854a7bca5b6ff54d632b993
DaniRomeroR <daniel.romero.rodriguez2@gmail.com>
2019-10-15 18:54:53 +0200

    segundo ejercicio

--- Ejer3LinqPractice_SecondTry/Program.cs
+++ Ejer3LinqPractice_SecondTry/Program.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Resources;
 using System.IO;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+
 
 namespace Ejer3LinqPractice_SecondTry
 {
@@ -13,8 +16,9 @@ namespace Ejer3LinqPractice_SecondTry
         static void Main(string[] args)
         {
             String archivo = File.ReadAllText("Cars.json");
+            string resxFile = "messages.resx";
             List<Car> coches = JsonConvert.DeserializeObject<List<Car>>(archivo);
-            Ejer4(coches);
+            Ejer5(coches,resxFile);
             Console.ReadKey();
         }
 
@@ -27,6 +31,24 @@ namespace Ejer3LinqPractice_SecondTry
             }
         }
 
-
+        public static void Ejer5(List<Car> coche, string resxFile)
+        {
+            Console.WriteLine($"Introduce una latitud para buscar:");
+            double latitud = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Introduce una longitud para buscar:");
+            double longitud = Convert.ToDouble(Console.ReadLine());
+            var ej5 = coche.Where(x => x.Color == "Turquoise" /*& x.location.Latitude==latitud & x.location.Longitude==longitud*/);
+            //using (ResourceReader resxReader = new ResourceReader(resxFile))
+            //{
+                foreach (var a in ej5)
+                {
+                    //La intención era mandar el objeto que cumpliera la lambda y mostrarlo a través de un fichero de recursos
+                    //y así no tener que redefinir métodos o escribir la misma salida, pero no he conseguido pasarle valores a
+                    //dicho archivo(aunque he visto que supuestamente se puede pero no lo e conseguido).
+                    Console.WriteLine(messages.mFabricante+a.Maker+messages.mModelo+a.Model);
+                    //Console.WriteLine($"Fabricante:{a.Maker}  Modelo:{a.Model}");
+                }
+            //}
+        }
     }
 }
