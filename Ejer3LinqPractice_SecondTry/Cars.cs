278af773393429f3982fc9d58af4634c1cf11441
Juan Carlos Cárdenas García <Jccarcia98@gmail.com>
2019-10-16 21:58:15 +0200

    ...

---
+++ Ejer3LinqPractice_SecondTry/Cars.cs
@@ -0,0 +1,14 @@
+﻿namespace Ejer3LinqPractice_SecondTry
+{
+
+    public class Cars
+    {
+        public int id { get; set; }
+        public string Maker { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+        public string Color { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
