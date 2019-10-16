beade9961d0431d353c2cc1a821741076628c73f
alejandro romero <aleja@DESKTOP-U2Q2U28>
2019-10-16 22:27:24 +0200

    2 primeros

---
+++ Ejer3LinqPractice_SecondTry/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer3LinqPractice_SecondTry
+{
+    class Car
+    {
+        public int id { get; set; }
+        public string maker { get; set; }
+        public string model { get; set; }
+        public int? year { get; set; }
+        public string color { get; set; }
+        public location location1 { get; set; }
+        public struct location
+        {
+            public double? Latitude { get; set; }
+
+            public double? Longitude { get; set; }
+        }
+    }
+}
+
--- Ejer3LinqPractice_SecondTry/Program.cs
+++ Ejer3LinqPractice_SecondTry/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,38 @@ namespace Ejer3LinqPractice_SecondTry
     {
         static void Main(string[] args)
         {
+            String archivo = File.ReadAllText("Cars.json");
+            List<Car> cochesito = JsonConvert.DeserializeObject<List<Car>>(archivo);
+            Ej4(cochesito);
+            Ej5(cochesito);
+            Console.ReadKey();
         }
+        public static void Ej4(List<Car> cochesio)
+        {
+            var colorCar = cochesio.Where(x => x.color == "Fuscia");
+            foreach (var a in colorCar)
+            {
+                Console.WriteLine("Marca: " + a.maker + " Modelo: " + a.model);
+            }
+        }
+
+       public static void Ej5(List<Car> cochesito)
+        {
+            Console.WriteLine("Inroduzca una Latitud: ");
+            Double latitud=Double.Parse(Console.ReadLine());
+            Console.WriteLine("Inroduzca una Longitud: ");
+            Double longi = Double.Parse(Console.ReadLine());
+            var colorCar = cochesio.Where(x => x.location1.Latitude == latitud && x.location1.Longitude == longi && x.color== "Turquoise");
+
+            foreach (var a in colorCar)
+            {
+                Console.WriteLine("Marca: " + a.maker + " Modelo: " + a.model);
+            }
+        }
+        public static void Ej7(List<Car> cochesito){
+
+
+        }
+
     }
 }
