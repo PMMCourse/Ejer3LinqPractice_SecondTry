0682f40887330046b8bfce342e64ef57ebcdb936
cvasilivg <vasikokk@gmail.com>
2019-10-16 20:34:04 -0400

    4, 5, 7 NI IDEA, 10 SIN TERMINAR

---
+++ Ejer3LinqPractice_SecondTry/Cars.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer3LinqPractice_SecondTry
+{
+    class Cars
+    {
+        public override string ToString()
+        {
+            return "Maker: " + this.Maker + "\nModel: " + this.Model + "\nYear: " + this.Year + "\nColor: " + this.Color + "\nLocation: " + this.gsLocation;
+        }
+        public string Maker { get; set; }
+        public string Model { get; set; }
+        public short? Year { get; set; }
+        public string Color { get; set; }
+        public Location gsLocation { get; set; }
+        public struct Location
+        {
+            public float? Latitude { get; set; }
+            public float? Longitude { get; set; }
+        }
+
+    }
+}
--- Ejer3LinqPractice_SecondTry/Program.cs
+++ Ejer3LinqPractice_SecondTry/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,42 @@ namespace Ejer3LinqPractice_SecondTry
 {
     class Program
     {
+        static List<Cars> listaCoche = new List<Cars>();
+
+        static Cars objCars = new Cars();
+
+        static List<Cars> ListaCoche()
+        {
+            using (StreamReader r = new StreamReader("Cars.json"))
+            {
+                string json = r.ReadToEnd();
+                listaCoche = JsonConvert.DeserializeObject<List<Cars>>(json);
+                return listaCoche;
+            }
+        }
+
+        static void Ejer4() => Console.WriteLine(listaCoche.Where(x => x.Color.Equals("Fuscia")));
+        static void Ejer5()
+        {
+            float? latitude, longitude;
+
+            Console.WriteLine("Latitud: ");
+            
+            latitude = float.Parse(Console.ReadLine());
+            latitude = objCars.gsLocation.Latitude;
+
+            Console.WriteLine("Longitud: ");
+
+            longitude = float.Parse(Console.ReadLine());
+            longitude = objCars.gsLocation.Longitude;
+
+            var f = listaCoche.Find(x => x.Color.Equals("Turquoise") && x.gsLocation.Latitude == latitude && x.gsLocation.Longitude == longitude);
+        }
+        static void Ejer7()
+        {
+            // Ni idea
+        }
+        static void Ejer10() => Console.WriteLine(listaCoche.GroupBy(x => x.Maker)); // Me falta mostrar colores sin duplicar
         static void Main(string[] args)
         {
         }
